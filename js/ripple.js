4e993e1819ff01650be9e6dd84f1a8720139ff90
François Pluchino <francois.pluchino@sonatra.com>
2017-04-30 16:07:39 +0200

    Call the plugin function with all arguments

--- js/ripple.js
+++ js/ripple.js
@@ -157,7 +157,9 @@
     // RIPPLE PLUGIN DEFINITION
     // ========================
 
-    function Plugin(option, value) {
+    function Plugin(option) {
+        var args = Array.prototype.slice.call(arguments, 1);
+
         return this.each(function () {
             var $this   = $(this),
                 data    = $this.data('st.ripple'),
@@ -173,7 +175,7 @@
             }
 
             if (typeof option === 'string') {
-                data[option](value);
+                data[option].apply(data, args);
             }
         });
     }
