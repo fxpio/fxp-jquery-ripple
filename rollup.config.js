f95a4d9d54c7d8cd4137b1654b319a1d186b3a90
François Pluchino <francois.pluchino@gmail.com>
2019-02-15 11:34:28 +0100

    Add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the Fxp package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import {uglify} from 'rollup-plugin-uglify';
+
+const baseConfig = {
+    input: 'js/ripple.js',
+    external: [
+        'jquery'
+    ],
+    plugins: [
+        resolve(),
+        commonjs({
+            include: 'node_modules/@fxp/jquery-pluginify/**'
+        }),
+        babel({
+            presets: [
+                '@babel/preset-env'
+            ]
+        })
+    ]
+};
+
+const uglifyConfig = {
+    compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+    }
+};
+
+export default [
+    {
+        ...baseConfig,
+        output: {
+            file: 'dist/ripple.js',
+            format: 'iife',
+            name: 'FxpRipple',
+            exports: 'named',
+            globals: {
+                jquery: 'jQuery'
+            }
+        }
+    },
+    {
+        ...baseConfig,
+        plugins: [...baseConfig.plugins, uglify(uglifyConfig)],
+        output: {
+            file: 'dist/ripple.min.js',
+            format: 'iife',
+            name: 'FxpRipple',
+            exports: 'named',
+            globals: {
+                jquery: 'jQuery'
+            }
+        }
+    }
+];
